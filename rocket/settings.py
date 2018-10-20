eb8be67416f0e94f6a0de1d35f6eac0e0cd1bc2b
Henrique Antunes <hinfo@gmx.com>
2018-10-20 20:34:24 -0300

    Project

--- rocket/settings.py
+++ rocket/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      #active this for deploy on production
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'rocket.urls'
